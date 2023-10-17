ff391106a16c7f8e9ce11d2b627773d6ad60a223
Brett Smith <brettsmith212@gmail.com>
2023-10-17 22:55:59 +0000

    test 2

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,14 @@ fn main() -> Result<()> {
             let mut file = File::open(&args[1])?;
             let mut header = [0; 100];
             file.read_exact(&mut header)?;
-
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
+            let mut b_tree_header = [0; 8];
+            file.read_exact(&mut b_tree_header)?;
+            let num_tables = u16::from_be_bytes([b_tree_header[3], b_tree_header[4]]);
 
             println!("database page size: {}", page_size);
+            println!("number of tables: {}", num_tables);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
